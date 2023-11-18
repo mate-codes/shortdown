271477806dac19b1f4debd254b1dcbdc8b8bae13
mate-codes | iMate <127528846+mate-codes@users.noreply.github.com>
2023-11-18 08:36:14 +0300

    Add files via upload

--- shortcut.py
+++ shortcut.py
@@ -17,15 +17,16 @@ root = tk.Tk()
 root.geometry('500x500')
 root.resizable(0, 0)
 root.title("Shortdown")
+root.configure(bg='#FFFFFF') 
 
 button_font = ("Helvetica", 12)
 
-button_pkill = tk.Button(root, text="Open Terminal", command=terminal, font=button_font)
-button_firefox = tk.Button(root, text="Open Browser", command=browser, font=button_font)
-button_thunar = tk.Button(root, text="Open Explorer", command=explorer, font=button_font)
+button_terminal = tk.Button(root, text="Open Terminal", command=terminal, font=button_font, bg='#FFFFFF', fg='black', bd=2, highlightbackground='#FFFFFF', activebackground='#FFFFFF')
+button_browser = tk.Button(root, text="Open Browser", command=browser, font=button_font, bg='#FFFFFF', fg='black', bd=2, highlightbackground='#FFFFFF', activebackground='#FFFFFF')
+button_explorer = tk.Button(root, text="Open Explorer", command=explorer, font=button_font, bg='#FFFFFF', fg='black', bd=2, highlightbackground='#FFFFFF', activebackground='#FFFFFF')
 
-button_pkill.pack(fill=tk.BOTH, expand=True, pady=10)
-button_firefox.pack(fill=tk.BOTH, expand=True, pady=10)
-button_thunar.pack(fill=tk.BOTH, expand=True, pady=10)
+button_terminal.pack(fill=tk.BOTH, expand=True, pady=10)
+button_browser.pack(fill=tk.BOTH, expand=True, pady=10)
+button_explorer.pack(fill=tk.BOTH, expand=True, pady=10)
 
 root.mainloop()
